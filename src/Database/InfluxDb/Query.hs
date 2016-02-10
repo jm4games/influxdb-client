0fb8b912db7a13733f80b8e11ff6840cf9200cc4
Joshua Montgomery <jm4games@live.com>
2016-02-10 08:20:21 -0800

    Started work on query generation.

--- src/Database/InfluxDb/Query.hs
+++ src/Database/InfluxDb/Query.hs
@@ -2,19 +2,26 @@
 
 module Database.InfluxDb.Query where
 
+import Data.ByteString (ByteString)
+import Data.ByteString.Builder (Builder, byteString, toLazyByteString)
+import Data.ByteString.Lazy (toStrict)
+import Data.List (foldl', intersperse)
+import Data.Monoid ((<>))
 import Data.Text (Text)
+import Data.Text.Encoding (encodeUtf8)
 
 import TextShow (TextShow, showt)
 
 data SelectStatement = Select !Target !From
                      | SelectConditional Target !From !WhereClause
 
-data Target = Count !Text
-            | Field !Text
-            | Max   !Text
-            | Mean  !Text
-            | Min   !Text
-            | Sum   !Text
+data Target = Count  !Text
+            | Field  !Text
+            | Targets ![Target]
+            | Max    !Text
+            | Mean   !Text
+            | Min    !Text
+            | Sum    !Text
 
 data From = FromShort Text
           | FromFull Text Text Text
@@ -81,6 +88,36 @@ where' :: SelectStatement -> WhereClause -> SelectStatement
 infix 1 `where'`
 where' (Select t f) w = SelectConditional t f w
 where' (SelectConditional t f w) w' = SelectConditional t f (And w w')
+
+toByteString :: SelectStatement -> ByteString
+toByteString (Select t f) = toStrict . toLazyByteString $ baseSelectBuilder t f
+toByteString (SelectConditional t f w) = toStrict . toLazyByteString $ 
+  baseSelectBuilder t f <> whereBuilder w
+
+baseSelectBuilder :: Target -> From -> Builder
+baseSelectBuilder t f = byteString "SELECT " <> targetBuilder t <> byteString " " <> fromBuilder f
+
+textBuilder :: Text -> Builder
+textBuilder = byteString . encodeUtf8
+
+targetBuilder :: Target -> Builder
+targetBuilder (Field f) = textBuilder f 
+targetBuilder (Count f) = byteString "COUNT(" <> textBuilder f <> byteString ")"
+targetBuilder (Max f) = byteString "MAX(" <> textBuilder f <> byteString ")"
+targetBuilder (Min f) = byteString "MIN(" <> textBuilder f <> byteString ")"
+targetBuilder (Mean f) = byteString "MEAN(" <> textBuilder f <> byteString ")"
+targetBuilder (Sum f) = byteString "SUM(" <> textBuilder f <> byteString ")"
+targetBuilder (Targets t) = foldl' (<>) mempty . intersperse (byteString ",") $ map targetBuilder t
+
+fromBuilder :: From -> Builder
+fromBuilder (FromShort t) = textBuilder t
+fromBuilder (FromFull db ret m) = 
+  textBuilder db <> byteString ".\"" <> textBuilder ret <> "\"." <> textBuilder m
+
+whereBuilder :: WhereClause -> Builder
+whereBuilder (Eq a b) = textBuilder a <> byteString "='" <> textBuilder b <> byteString "'"
+whereBuilder (Gt a b) = textBuilder a <> byteString ">'" <> textBuilder b <> byteString "'"
+whereBuilder (Lt a b) = textBuilder a <> byteString "<'" <> textBuilder b <> byteString "'"
 
 temp :: SelectStatement
 temp = select (maxT "*") (from "test") `where'` "temp" .>. (5::Int) .&&. "temp" .<. (10::Int)
